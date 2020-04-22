54abb19ef5d3c08d594960a0211ba3b979705a7f
matthias <uso.cosmo.ray@gmail.com>
2020-04-22 18:22:53 +0200

    more song and unused map next
    Signed-off-by: matthias <uso.cosmo.ray@gmail.com>

--- js/game_level/patern01.js
+++ js/game_level/patern01.js
@@ -9,16 +9,16 @@ patern1.addMob(0, ship_testing, verticalLine, 170);
 patern1.addMob(0, ship_testing, verticalLine, 220);
 //Vague II
 patern1.addMob(10, fast_ship_testing, slow_zigzag, 100);
-patern1.addMob(10, fast_ship_testing, slow_zigzag, 200);
-patern1.addMob(10, ship_testing, zigzag, 100);
-patern1.addMob(10, ship_testing, zigzag, 200);
+patern1.addMob(10, fast_ship_testing, slow_zigzag, 500);
+patern1.addMob(10, ship_testing, zigzag, 300);
+patern1.addMob(10, ship_testing, zigzag, 400);
 //Vague III
-patern1.addMob(20, ship_testing, verticalLine, 70);
-patern1.addMob(20, ship_testing, verticalLine, 120);
-patern1.addMob(20, ship_testing, verticalLine, 170);
-patern1.addMob(20, ship_testing, verticalLine, 220);
+patern1.addMob(20, ship_testing, verticalLine, 270);
+patern1.addMob(20, ship_testing, verticalLine, 320);
+patern1.addMob(20, ship_testing, verticalLine, 370);
+patern1.addMob(20, ship_testing, verticalLine, 420);
 //Vague IV
 patern1.addMob(40, ship_testing, verticalLine, 70);
-patern1.addMob(40, ship_testing, verticalLine, 120);
-patern1.addMob(40, ship_testing, verticalLine, 170);
 patern1.addMob(40, ship_testing, verticalLine, 220);
+patern1.addMob(40, ship_testing, verticalLine, 370);
+patern1.addMob(40, ship_testing, verticalLine, 520);
