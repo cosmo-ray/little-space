2b6b5c83e808082b013f54896805897e7b93c123
matthias <uso.cosmo.ray@gmail.com>
2020-04-22 18:57:02 +0200

    patern 3 ?
    Signed-off-by: matthias <uso.cosmo.ray@gmail.com>

--- js/game_level/patern02.js
+++ js/game_level/patern02.js
@@ -9,23 +9,24 @@ patern2.addMob(0, ship_testing, verticalLine, 170);
 patern2.addMob(0, ship_testing, verticalLine, 220);
 patern2.addMob(0, ship_testing, verticalLine, 280);
 
-patern2.addMob(3, ship_testing, verticalLine, 10);
-patern2.addMob(3, ship_testing, verticalLine, 73);
-patern2.addMob(3, ship_testing, verticalLine, 120);
-patern2.addMob(3, ship_testing, verticalLine, 173);
-patern2.addMob(3, ship_testing, verticalLine, 220);
-patern2.addMob(3, ship_testing, verticalLine, 280);
+patern2.addMob(3, ship_testing, verticalLine, 210);
+patern2.addMob(3, ship_testing, verticalLine, 273);
+patern2.addMob(3, ship_testing, verticalLine, 320);
+patern2.addMob(3, ship_testing, verticalLine, 373);
+patern2.addMob(3, ship_testing, verticalLine, 420);
+patern2.addMob(3, ship_testing, verticalLine, 480);
 
-patern2.addMob(6, ship_testing, verticalLine, 10);
-patern2.addMob(6, ship_testing, verticalLine, 76);
-patern2.addMob(6, ship_testing, verticalLine, 120);
-patern2.addMob(6, ship_testing, verticalLine, 176);
-patern2.addMob(6, ship_testing, verticalLine, 220);
-patern2.addMob(6, ship_testing, verticalLine, 280);
+patern2.addMob(6, ship_testing, verticalLine, 100);
+patern2.addMob(6, ship_testing, verticalLine, 076);
+patern2.addMob(6, ship_testing, verticalLine, 320);
+patern2.addMob(6, ship_testing, verticalLine, 376);
+patern2.addMob(6, ship_testing, verticalLine, 520);
+patern2.addMob(6, ship_testing, verticalLine, 580);
 
-patern2.addMob(9, ship_testing, verticalLine, 10);
-patern2.addMob(9, ship_testing, verticalLine, 79);
-patern2.addMob(9, ship_testing, verticalLine, 120);
+patern2.addMob(9, ship_testing, verticalLine, 70);
 patern2.addMob(9, ship_testing, verticalLine, 179);
-patern2.addMob(9, ship_testing, verticalLine, 220);
-patern2.addMob(9, ship_testing, verticalLine, 280);
+patern2.addMob(9, ship_testing, verticalLine, 279);
+patern2.addMob(9, ship_testing, verticalLine, 320);
+patern2.addMob(9, ship_testing, verticalLine, 479);
+patern2.addMob(9, ship_testing, verticalLine, 520);
+
---
+++ js/game_level/patern03.js
@@ -0,0 +1,23 @@
+var patern3 = new Patern;
+paterns["patern3"] = patern3;
+
+//Vague VI
+patern3.addMob(1, ship_testing, verticalLine, 30);
+patern3.addMob(2, ship_testing, verticalLine, 60);
+patern3.addMob(3, ship_testing, verticalLine, 90);
+patern3.addMob(4, ship_testing, verticalLine, 120);
+patern3.addMob(5, ship_testing, verticalLine, 150);
+patern3.addMob(6, ship_testing, verticalLine, 180);
+patern3.addMob(7, ship_testing, verticalLine, 210);
+patern3.addMob(5, ship_testing, verticalLine, 240);
+patern3.addMob(6, ship_testing, verticalLine, 270);
+patern3.addMob(7, ship_testing, verticalLine, 300);
+patern3.addMob(8, ship_testing, verticalLine, 330);
+patern3.addMob(9, ship_testing, verticalLine, 360);
+patern3.addMob(10, ship_testing, verticalLine, 390);
+patern3.addMob(11, ship_testing, verticalLine, 420);
+patern3.addMob(12, ship_testing, verticalLine, 450);
+patern3.addMob(13, ship_testing, verticalLine, 480);
+patern3.addMob(14, ship_testing, verticalLine, 510);
+patern3.addMob(15, ship_testing, verticalLine, 540);
+patern3.addMob(16, ship_testing, verticalLine, 570);
